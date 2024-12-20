30cd7c2007baf1f0efb17cfd3c0d10d8ad553416
Igor Vinokur <vinokurig@gmail.com>
2024-12-20 08:41:19 +0000

    commit

---
+++ src/main/java/com/ivinokur/app/Day3.java
@@ -0,0 +1,50 @@
+package com.ivinokur.app;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.io.Resources;
+
+public class Day3 {
+    private static String inputData;
+    
+    public Day3() {
+        System.out.println("\n>>>>>> Day 3: ");
+        try {
+			inputData = getInputData();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+
+    public static void part1() {
+        Pattern pattern = Pattern.compile("mul\\((?<value1>[0-9]*),(?<value2>[0-9]*)\\)");
+        Matcher matcher = pattern.matcher(inputData);
+        int result = 0;
+        while (matcher.find()) {
+            int value1 = Integer.valueOf(matcher.group("value1"));
+            int value2 = Integer.valueOf(matcher.group("value2"));
+            result += value1 * value2;
+          }
+        System.out.println(">>>>>> part 1: " + result);
+    }
+
+    public static void part2() {
+        Pattern pattern = Pattern.compile("mul\\((?<value1>[0-9]*),(?<value2>[0-9]*)\\)");
+        Matcher matcher = pattern.matcher(inputData);
+        int result = 0;
+        while (matcher.find()) {
+            int value1 = Integer.valueOf(matcher.group("value1"));
+            int value2 = Integer.valueOf(matcher.group("value2"));
+            result += value1 * value2;
+          }
+        System.out.println(">>>>>> part 2: " + result);
+    }
+
+    private static String getInputData() throws IOException {
+        return Resources.toString(Resources.getResource("day-3.txt"), UTF_8);
+    }
+}
